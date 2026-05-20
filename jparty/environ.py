51d39dbb011dddee1ca225ea1dacf8fba4816528
Ben Finch <benhome@Bens-MacBook-Pro.local>
2026-05-19 22:22:58 -0600

    Github pages

--- jparty/environ.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-if getattr(sys, "frozen", False):
-    # For PyInstaller, _MEIPASS is where bundled resources are.
-    # The application itself might be in .app/Contents/MacOS
-    root = getattr(sys, "_MEIPASS", os.path.dirname(sys.executable))
-else:
-    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
